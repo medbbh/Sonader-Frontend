c4a665911d73ca87cd1c07542e614f9b3f7e0cd2
medbbh <mbabah3450@gmail.com>
2023-12-16 18:47:48 +0000

    first commit

---
+++ evaluation et suivi/statistique.js
@@ -0,0 +1,101 @@
+const data = {
+    'realisés': 62,
+    'en cours': 13,
+    'prevus': 23,
+
+};
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: Object.keys(data),
+        datasets: [
+            {
+                label: 'Number of GitHub Stars',
+                data: Object.values(data),
+                backgroundColor: ['#119706','#90EE90','#ececec'],
+                
+            },
+        ],
+    },
+
+    options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'left',
+          },
+          title: {
+            display: true,
+          },
+          
+        },
+
+      },
+});
+
+
+const barChartData = {
+  'a': 62,
+  'b': 13,
+  'c': 23,
+  'd': 13,
+  'e': 62,
+  'f': 13,
+  'g': 23,
+  'h': 13,
+  'i': 62,
+  'j': 13,
+  'k': 23,
+  'l': 13,
+
+
+};
+const barChart = document.getElementById('myBarChart').getContext('2d');
+const myBarChart = new Chart(barChart, {
+    type: 'bar',
+    data: {
+        labels: Object.keys(barChartData),
+        datasets: [
+            {
+                data: Object.values(barChartData),
+                backgroundColor: '#119706',
+                
+            },
+        ],
+    },
+
+    options: {
+        responsive: true,
+        plugins: {
+          title: {
+            display: true,
+          },
+          legend: {
+            display: false, // Set to true if you want to display the legend
+        }
+          
+        },
+        borderRadius : 50,
+        barThickness : 15,
+        scales: {
+          display : false,
+          x: {
+            grid: {
+              display: false
+            },
+            
+          },
+          y: {
+            grid: {
+              display: false
+            },
+           
+          },
+        
+          },
+      }
+        
+
+});
