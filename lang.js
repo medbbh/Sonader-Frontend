72fb28509e2e35b120fc31be97f32c8f97d2c4aa
medbbh <mbabah3450@gmail.com>
2023-12-23 22:22:27 +0000

    'multilanguage'

---
+++ lang.js
@@ -0,0 +1,28 @@
+import translations from "./translations.js";
+
+const body = document.body;
+
+const languageSelector = document.querySelector("select");
+
+languageSelector.addEventListener('change', (event) => {
+
+    setLanguage(event.target.value)
+    localStorage.setItem("lang",event.target.value)
+
+})
+
+    
+ 
+
+document.addEventListener("DOMContentLoaded",()=>{
+    setLanguage(localStorage.getItem("lang") || "fr")
+})
+
+const setLanguage = (language) => {
+    const elements = document.querySelectorAll('[data-i18n]')
+    elements.forEach((element) => {
+        const translationKey = element.getAttribute("data-i18n")
+        element.textContent = translations[language][translationKey]
+    })
+    document.dir = language === "ar" ? "rtl" : "ltr"
+}
